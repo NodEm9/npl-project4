d1e4e94f06bae636570db65aa59b28b92b8055aa
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-02 02:26:10 +0100

    create a json test API, for testing the API call

---
+++ src/server/mockAPI.js
@@ -0,0 +1,7 @@
+let json = {
+          "title": "test json response",
+          "message": "Printing to tell you, json response is working fine",
+          "time": "now"
+}
+
+module.exports = json
