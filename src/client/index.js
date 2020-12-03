3c504a508c054cc3aea07baf21df0fd356b5095e
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-03 23:39:25 +0100

    wrote a  module.hot function to enable it's webpack setup

--- src/client/index.js
+++ src/client/index.js
@@ -1,36 +1,44 @@
 import { handleSubmit } from './js/formHandler';
 import { checkForName } from './js/nameChecker';
 
-import './styles/base.css';
-
-
+import './styles/base.css'; 
+       
 console.log(checkForName);
 
-export {
+if (module.hot) {
+          module.hot.accept('./js/formHandler.js', function() {
+            console.log('Accepting the updated handleSubmit module!');
+            handleSubmit();
+          })
+}     
+
+export {               
           handleSubmit,
           checkForName
-}
+};
 
-function component() {
 
-          console.log('The components will be output here!');
 
-          const element = document.createElement('div');
+// function component() {
 
-          const btn = document.createElement('button');
+//           console.log('The components will be output here!');
 
-          const para = document.createElement('p');
+//           const element = document.createElement('div');
+  
+//           const btn = document.createElement('button');
 
-          para.textContent =  'The language proccessing web application';
+//           const para = document.createElement('p');
 
-          btn.innerHTML = "Click me"
+//           para.textContent =  'The language proccessing web application';
 
-          // btn.onclick = printMe;
+//           btn.innerHTML = "Click me"
 
-          // element.appendChild(btn)
+//           // btn.onclick = printMe;
 
-          element.appendChild(para)
+//           element.appendChild(btn)
+
+//           element.appendChild(para)
  
-          return element;
-}
-document.body.appendChild(component());
+//           return element;
+// }
+// document.body.appendChild(component());
