13ee5c63b4da94c4067e1d8470d8912f6cac5da4
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-03 23:44:43 +0100

    no major changes

--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -2,6 +2,5 @@ let json = {
           "title": "test json response",
           "message": "Printing to tell you, json response is working fine",
           "time": "now"
-}
-
-module.exports = json
+} 
+module.exports = json;      
