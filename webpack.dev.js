b08d7dc51560c2e47ad5081dffa6b461a867a74b
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2020-12-03 23:50:28 +0100

    create webpack.dev.js file and added new  configurations

---
+++ webpack.dev.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+
+
+module.exports = {
+          mode: 'development',
+          entry:  './src/client/index.js',
+
+          output: { 
+            libraryTarget: 'var',
+            library: 'Client'
+      },
+          devtool: 'source-map',
+          devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000,
+            hot: true
+          },
+ module: {
+   rules: [
+
+          {
+             test: /\.js$/,
+             exclude: /(node_modules|bower_components)/,
+             use: {
+                    loader: 'babel-loader',
+                    options: {  
+                               
+                    plugins: ['@babel/plugin-transform-runtime']
+                  }
+          }
+         },
+          {
+                      test: /\.css$/i,
+                      use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.html$/i,
+            loader: 'html-loader',
+          },
+         
+        ]
+       
+      },
+      plugins: [
+                new CleanWebpackPlugin({
+                     // Simulate the removal of files
+                dry: true,
+                // Write Logs to Console
+                verbose: true,
+                // Automatically remove all unused webpack assets on rebuild
+                cleanStaleWebpackAssets: true,
+                protectWebpackAssets: false
+                }),
+                new MiniCssExtractPlugin({}),
+                new HtmlWebpackPlugin({
+                              template: './src/client/views/index.html',
+                              filename: './index.html'
+          }),
+       ],
+       
+}
+
