9aed6fda2af3a618639cef435be26d0cd4ae156d
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2021-01-20 14:08:21 +0100

    created meassage alert file and added code

---
+++ src/client/js/alert-message-service.js
@@ -0,0 +1,32 @@
+const { isValidUrl } = require("./urlIsValid");
+
+function displayMessage(msgText, msgType) {
+const html = document.querySelector('#panel');
+
+const panel = document.createElement('div');
+panel.setAttribute('class', 'msgBox');
+html.appendChild(panel);
+
+const msg = document.createElement('p');
+msg.textContent = msgText;
+panel.appendChild(msg);   
+
+const btn = document.getElementById('generate');
+
+btn.onclick = function() {
+          displayMessage('Invalid Url: please insert a valid Url' `${!isValidUrl}`);
+          displayMessage('URL is valid' `${isValidUrl}`);
+        };
+
+if (msgType !== isValidUrl) {
+  // msg.style.backgroundImage = 'url(icons/warning.png)';
+  panel.style.backgroundColor = 'red';
+} else if (msgType === isValidUrl) {
+  // msg.style.backgroundImage = 'url(icons/chat.png)';
+  panel.style.backgroundColor = 'blue';
+} else {
+  msg.style.paddingLeft = '20px';
+}
+}
+
+export { displayMessage }
