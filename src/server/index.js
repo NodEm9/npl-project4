3ff3d34795a4d12e6cf64c488e8edc4c4bce62f9
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2021-01-21 16:02:06 +0100

    debugged code

--- src/server/index.js
+++ src/server/index.js
@@ -2,27 +2,25 @@ let projectData = {};
 
 //Require dotenv define in the .env file
 
-// if(process.env.NODE_ENV !== 'production'){
-//           require('dotenv').config()
-// }
-
-const dotenv = require('dotenv');
-const API_KEY = process.env.API_KEY;
+const path = require('path')
+const dotenv = require('dotenv')
 dotenv.config()
+const API_KEY = process.env.API_KEY;
+
 
 //Set the url in a variable name baseURL const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=';
 
 const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1?key=';
 
-//Require Express and body-parser
+//Require Express 
 const express = require('express');
 
 //Create an instance of the app
 const app = express();
 
+//Require bodyparser
 const bodyParser = require('body-parser');
 app.use(bodyParser.text()); 
-// app.use(express.json()); 
 
 //Middleware 
 const cors = require('cors');
@@ -37,38 +35,26 @@ const port = process.env.PORT || 8001;
 
 
 app.get('*',  function (req, res) {    
-          res.sendFile(path.join(__dirname + 'dist/index.html'))          
+             res.sendFile('dist/index.html')          
 });
 
 app.get('/all', (req, res) => {
-          res.send(projectData)
+             res.send(projectData)
 })
-
+   
 app.post('/analyse', async (req, res) => {
-          console.log(req.body)
-          const response = await fetch( `${baseUrl}${API_KEY}&lang=auto&txt=${req.body.text}&model=general&url=${req.body}`);
+             console.log(req.body)
+             const response = await fetch(`${baseUrl}${API_KEY}&lang=en&txt=${req.body.text}&model=general&url=${req.body.url}`);
 
           try {
-
-                    const data = await response.text();
-        
-                    projectData = data;
-                    res.send(projectData)
-                    res.json()
-                    // res.send(data)  
+             const data = await response.text();            
+             res.send(data)
+             res.json()
           } catch (error) {
-                    console.log(error.message)
+             console.log(error.message)
           }
 })  
        
-
-//            axios({
-//                     url: url,
-//                     responseType: 'text'  
-//           }).then(data => data.text(url))
-//           .catch(err => data.status(400).json('Error: ' + err));    
-
-// })
  
 app.listen(port, () => {     
           console.log(`Server listening on port: ${port}`); 
