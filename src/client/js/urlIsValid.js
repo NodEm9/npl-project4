8d329ac25c5351b80bbc86da6d61bb1c86946fb7
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2021-01-21 16:08:31 +0100

    debugged code

--- src/client/js/urlIsValid.js
+++ src/client/js/urlIsValid.js
@@ -25,20 +25,3 @@ export {isValidUrl }
 
 
 
-
-//  function checkForName(inputText) {
-//           console.log("::: Running checkForName :::", inputText);
-//           let names = [
-//               "Picard",
-//               "Janeway",
-//               "Kirk",
-//               "Archer",  
-//               "Georgiou" 
-//           ]
-      
-//           if(names.includes(inputText)) {
-//               alert("Welcome, Captain!")
-//           }
-//       }
-      
-//       export { checkForName } 
