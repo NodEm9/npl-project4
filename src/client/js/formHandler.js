28dd4439b9cea16f4c38bc0401c9bd85dd6f3b1d
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2021-01-22 11:27:48 +0100

    fetched api and updated UI with fetch data

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,10 +1,11 @@
 import { isValidUrl } from "./urlIsValid";
 import { displayMessage } from "./alert-message-service"
-// import { checkInput } from "./inputAreValid";
+import { checkInput } from "./inputAreValid";
+import fetch from "node-fetch";
 
 document.getElementById('generate').addEventListener('click', handleSubmit);  
 
-
+ 
  function handleSubmit(event) {
         event.preventDefault()
         
@@ -20,80 +21,30 @@ document.getElementById('generate').addEventListener('click', handleSubmit);
             method: 'POST',
             credentials: 'same-origin', 
             headers: {
-                // 'Content-Type': 'application/json', 
-                'Content-Type': 'text/plain'
+                'Content-Type': 'application/json', 
             }, 
-            body: url,// the Content-Type matches the body
-            
-     }).then(res => res.text())
-    .then(text => {
-            console.log(text)
-            document.querySelector('.score_tag').innerHTML = `score_tag: ${score_tag(text.score_tag)}`
-            document.querySelector('.agreement').innerHTML = `agreement: ${agreement(text.agreement)}`
-            document.querySelector('.subjectivity').innerHTML = `subjectivity: ${subjectivity(text.subjectivity)}`
-            document.querySelector('.confidence').innerHTML = `confidence: ${confidence(text.confidence)}`
-            document.querySelector('.irony').innerHTML = `irony: ${irony(text.irony)}`
+            body: JSON.stringify({ url: inputText}),// the Content-Type matches the body
+             
+     }).then(res => res.json())
+    .then(data => {
+            console.log(data)
+            document.querySelector('.score_tag').innerHTML = `score_tag: ${data.score_tag}`
+            document.querySelector('.agreement').innerHTML = `agreement: ${data.agreement}`
+            document.querySelector('.subjectivity').innerHTML = `subjectivity: ${data.subjectivity}`
+            document.querySelector('.confidence').innerHTML = `confidence: ${data.confidence}`
+            document.querySelector('.irony').innerHTML = `irony: ${data.irony}`
 
-          return text
-        })     
+          return data  
+        })    
   }else{
             document.querySelector('.errorMsg').innerHTML = displayMessage('URL/Text are not readable. Enter a valid text/url');
             console.log('URL/Text are not readable. Enter a valid text/url')
 
   }
-
+            
 }
 
     
 
-      const score_tag = (score_tag) => {
-        if (score_tag === "P+" || score_tag === "P") {
-          return "Positive";
-        } else if (score_tag === "N+" || score_tag === "N") {
-          return "Negative";
-        } else if (score_tag === "NEU") {
-          return "Neutral";
-        } else {
-          return "NON";
-        }    
-      };
-
-      //Check if there is an agreement
-      const agreement = (agreement) => {
-          if(agreement === "samePolarity") {
-                return "AGREEMENT"
-          }else if(agreement !== "samePolarity"){
-               return "DISAGREEMENT"
-          }
-      };
-      
-      const subjectivity = (article) => {
-          if(article === "OBJECTIVE") {
-              return 'OBJECTIVE'
-          }else if(article === "SUBJECTIVE"){
-              return 'SUBJECTIVE'
-          }else {
-              return 'Article has neither OBJECTIVE nor SUBJECTIVE marks'
-          }
-
-      }
-
-      const confidence = (confidence) => {
-          if(confidence > 0){
-              return Math.random(confidence);
-          }else{
-              return 'NO CONFIDENCE'
-          }
-      }
-      
-      const irony = (ironic) => {
-          if(!ironic){
-                return "NONIRONIC"
-          }else{
-              return "IRONIC"
-          }
-      }
- 
-
-export { handleSubmit, score_tag, agreement, subjectivity, confidence, irony }
+export { handleSubmit }
     
