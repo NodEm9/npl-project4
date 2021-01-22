2e7defaa9450b494bbae2bb06301162ca3d41e4f
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2021-01-22 11:32:01 +0100

    debugged code

--- src/client/js/alert-message-service.js
+++ src/client/js/alert-message-service.js
@@ -1,6 +1,6 @@
+import { isValidUrl } from "./urlIsValid";
 import { checkInput } from "./inputAreValid";
 
-import { isValidUrl } from "./urlIsValid";
 
 function displayMessage(msgText, msgType) {
 const html = document.querySelector('#panel');
@@ -13,18 +13,16 @@ const msg = document.createElement('p');
 msg.textContent = msgText;      
 panel.appendChild(msg);    
 
-const btn = document.getElementById('generate');
+// const btn = document.getElementById('generate');
 
-btn.onclick = function() {
-          displayMessage('Invalid Url: please insert a valid Url' `${!isValidUrl}`);
-          displayMessage('URL is valid' `${isValidUrl}`);
-        };
+// btn.onclick = function() {
+//           displayMessage('Invalid Url: please insert a valid Url' `${!isValidUrl}`);
+//           displayMessage('URL is valid' `${isValidUrl}`);
+//         };
 
 if (msgType !== isValidUrl || msgType !== checkInput) {
-  // msg.style.backgroundImage = 'url(icons/warning.png)';
-  panel.style.backgroundColor = 'red';
-} else if (msgType === isValidUrl || msgType === checkInput) {
-  // msg.style.backgroundImage = 'url(icons/chat.png)';
+  panel.style.backgroundColor = 'skyblue';
+}else if(msgType === isValidUrl || msgType === checkInput) {
   panel.style.backgroundColor = 'blue';
 } else {
   msg.style.paddingLeft = '20px';
