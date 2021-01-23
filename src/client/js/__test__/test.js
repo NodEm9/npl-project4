dd2cbdd0b6eeee438c007530fb6fdfed452ed019
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2021-01-23 14:34:40 +0100

    condected check on url input

--- src/client/js/__test__/test.js
+++ src/client/js/__test__/test.js
@@ -1,11 +1,4 @@
 const { library } = require("webpack")
-const { handleSubmit } = require("../formHandler")
-const { checkForName } = require("../nameChecker")
-
-test('my function', () => {
-          expect(handleSubmit).toBe(handleSubmit)
-          expect(checkForName).not.toBe(null)
-})
 
 test('library is defined with the name Client', () => {
           const clientLibrary = library.client;
