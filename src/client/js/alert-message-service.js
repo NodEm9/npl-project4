b18a174a971e076a33bfc204640f6033aee42318
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2021-01-23 12:58:21 +0100

    debugged code and styled the panel

--- src/client/js/alert-message-service.js
+++ src/client/js/alert-message-service.js
@@ -13,20 +13,21 @@ const msg = document.createElement('p');
 msg.textContent = msgText;      
 panel.appendChild(msg);    
 
-// const btn = document.getElementById('generate');
+const btn = document.getElementById('generate');
 
-// btn.onclick = function() {
-//           displayMessage('Invalid Url: please insert a valid Url' `${!isValidUrl}`);
-//           displayMessage('URL is valid' `${isValidUrl}`);
-//         };
+btn.onclick = function() {
+          displayMessage('Invalid Url: please insert a valid Url' `${!isValidUrl}`);
+          displayMessage('URL is valid' `${isValidUrl}`);
+        };
 
-if (msgType !== isValidUrl || msgType !== checkInput) {
-  panel.style.backgroundColor = 'skyblue';
-}else if(msgType === isValidUrl || msgType === checkInput) {
-  panel.style.backgroundColor = 'blue';
-} else {
-  msg.style.paddingLeft = '20px';
-}
+  if (msgType === isValidUrl) {
+    panel.style.backgroundColor = 'skyblue';
+  }else if(msgType === checkInput) {
+    panel.style.backgroundColor = 'aqua';
+  } else {
+    panel.style.backgroundColor = 'red'
+    msg.style.paddingLeft = '20px';
+  }
 }
 
 export { displayMessage }
