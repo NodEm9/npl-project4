537f639527ced80fa919f7e3d472e7962c30ad0f
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2021-01-23 13:48:43 +0100

    debugged code and added comments

--- src/client/index.js
+++ src/client/index.js
@@ -1,21 +1,24 @@
+//Import all files into the index.js the will run the app
 import { handleSubmit } from './js/formHandler';
-import { isValidUrl } from './js/urlIsValid'; 
+import { isValidUrl, checkUrl } from './js/urlIsValid'; 
 import { displayMessage } from './js/alert-message-service';    
-import { checkInput } from './js/inputAreValid';
 
+//import the base.scss file where all partial scss file are already integrated 
 import './styles/base.scss';  
         
-         
+
+//Configure the Hot Module Replacement
 if (module.hot) {
           module.hot.accept('./js/formHandler.js', function() {
-            console.log('Accepting the updated handleSubmit module!');
-            handleSubmit();
-          })  
+          console.log('Accepting the updated handleSubmit module!');
+          handleSubmit();
+  })  
 }                 
 
+//Export import files as well
 export { 
           handleSubmit,                                     
           isValidUrl,
           displayMessage,
-          checkInput   
+          checkUrl   
  };         
